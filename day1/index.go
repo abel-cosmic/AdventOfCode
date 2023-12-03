7a8c75abb841c16455ecd49fb20defc5e80fc3cd
Abel Shibabaw <abelshibabaw291@gmail.com>
2023-12-03 20:03:12 +0300

    pointers

--- day1/index.go
+++ day1/index.go
@@ -4,20 +4,13 @@ import "fmt"
 
 // learning go on the way
 /**
-var nameOfVariable typeOfVariable = value
-var nameOfVariable = value
-nameOfVariable := value
-
+ * @Description: main function
+ * @param
+ * @return
 **/
 func main() {
-	fmt.Println("Hello, Go!")
-	// var integer int = 1
-	// integers := 1
-	// var integere = 1
-	// integer, integere, integers := 2, 3, 4
-	// fmt.Println(integers)
-	// fmt.Println(integer)
-	// fmt.Println(integere)
-	// fmt.Println(integere, integer, integers)
-
+	x := 1
+	p := &x
+	fmt.Println(*p)
+	fmt.Println(p)
 }
