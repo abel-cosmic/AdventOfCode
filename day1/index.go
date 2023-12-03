c1c1dca5bf7b9f5548ba5c1a7e13368cc0d76a6b
Abel Shibabaw <abelshibabaw291@gmail.com>
2023-12-03 19:59:50 +0300

    added comment to show the different way to declare variables

---
+++ day1/index.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// learning go on the way
+/**
+var nameOfVariable typeOfVariable = value
+var nameOfVariable = value
+nameOfVariable := value
+
+**/
+func main() {
+	fmt.Println("Hello, Go!")
+	// var integer int = 1
+	// integers := 1
+	// var integere = 1
+	// integer, integere, integers := 2, 3, 4
+	// fmt.Println(integers)
+	// fmt.Println(integer)
+	// fmt.Println(integere)
+	// fmt.Println(integere, integer, integers)
+
+}
